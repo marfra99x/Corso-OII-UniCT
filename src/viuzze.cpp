d06671cb5af7ce7f7469ab1f91f44483ed5bd011
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-04-15 21:14:23 +0200

    Add files via upload

---
+++ src/viuzze.cpp
@@ -0,0 +1,73 @@
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <limits>
+
+using namespace std;
+
+struct Arco {
+    int dest, peso;
+};
+struct Nodo {
+    vector<Arco> adj;
+};
+
+const int INF = numeric_limits<int>::max();
+
+// time complexity: O(V + E log(V))
+// can you design a better solution?
+// suggestion: O(V + E)
+int dijkstra(int n, int src, int dest, Nodo *g) {
+    priority_queue<pair<int, int>> pq;
+    vector<int> dist(n, INF);
+
+    pq.push({0, src});
+    dist[src] = 0;
+
+    while (!pq.empty()) {
+
+        int u = pq.top().second;
+        pq.pop();
+
+        for (auto v: g[u].adj) {
+            if (dist[v.dest] > dist[u] + v.peso) {
+                dist[v.dest] = dist[u] + v.peso;
+                pq.push({dist[v.dest], v.dest});
+            }
+        }
+
+    }
+
+    return (dist[dest] == INF ? -1 : dist[dest]);
+}
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    for (int task = 1; task <= 100; task++) {
+        int N;
+        in >> N;
+
+        int P, L;
+        in >> P >> L;
+
+        Nodo *G = new Nodo[N];
+
+        for (int i = 0, x, y; i < P+L; i++) {
+            in >> x >> y;
+            x--, y--;
+
+            G[x].adj.push_back({y, int(i >= P)});
+            G[y].adj.push_back({x, int(i >= P)});
+        }
+
+        out << dijkstra(N, 0, N-1, G) << endl;
+
+    }
+
+    in.close();
+    out.close();
+
+    return 0;
+}
